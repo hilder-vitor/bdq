2b38845e57621b87c9fcea15133cdb28fb01efe4
vitorlima <vitor_lp@yahoo.com>
2014-05-10 21:56:27 -0300

    Identação e outras mudanças pequenas

--- iniciaAmbiente.sh
+++ iniciaAmbiente.sh
@@ -7,3 +7,22 @@ mkdir composer/
 
 mv composer.phar composer/
 
+cd composer
+
+# cria o arquivo com as dependências
+echo '
+{
+	"require": {
+		"monolog/monolog": "1.0.*"
+	}
+}
+
+{
+	"require-dev": {
+         "phpunit/phpunit": "4.1.*"
+	}
+}' > composer.json
+
+
+# instala as dependências
+php composer.phar install
