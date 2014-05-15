4ba5ce97b8f1fa5817f9693d318af4be2c419669
vitorlima <vitor_lp@yahoo.com>
2014-05-15 08:08:08 -0300

    Adicionei tratamento de filtros à classe Questao
    Um campo $filtros e métodos para adicionar um filtro.

--- fonte/classes/Questao.php
+++ fonte/classes/Questao.php
@@ -2,10 +2,26 @@
 namespace classes_;
 
 class Questao {
-    
+
+    /* definindo constanstes para os tipos de questão */
+    const QUESTAO_ALTERNATIVA = 1;
+    const QUESTAO_DISSERTATIVA = 2;
+
     protected $id;
     protected $enunciado;
     protected $ano;
+    /* vetor dos filtros associados à questão
+     *  indexados pelo id do filtro
+     *  (guardando apenas o 1º nível. Os filhos estão nos vetores de
+     *  filhos dos próprios filtros)
+     */
+    protected $filtros;
+    
+    public function __construct($id, $enunciado, $ano) {
+        $this->ano = $ano;
+        $this->enunciado = $enunciado;
+        $this->id = $id;
+    }
     
     public function getId() {
         return $this->id;
@@ -30,5 +46,31 @@ class Questao {
     public function setAno($ano) {
         $this->ano = $ano;
     }
-
+    
+    private function adicionaFiltroRec(Filtro $f, $filtros){
+        if (isset($filtros[$f->getId()])){
+	   return true;
+        }
+        // se o filtro passado for filho de algum filtro
+        if (isset($filtros[$f->getIdPai()])){
+	   $filtro = $filtros[$f->getIdPai()];
+	   $filtro->adicionaFilho($f);
+	   return true;
+        }
+        // tenta adicionar o filtro na sub-árvore a partir de algum filtro
+        foreach ($filtros as $filtro){
+	   if ($this->adicionaFiltroRec($f, $filtro->getFilho())){
+	       return true;
+	   }
+        }
+        return false;
+    }
+    
+    public function adicionaFiltro(Filtro $f){
+        // tenta adicionar o filtro como filho de algum filtro já existente
+        $adicionou = $this->adicionaFiltroRec($f, $this->filtros);
+        if (!$adicionou){ // se não adicionou
+	   $this->filtros[$f->getId()] = $f;
+        }
+    }
 }
