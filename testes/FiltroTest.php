2743018522c9ba6d8ad209a5a567334a5f30029d
vitorlima <vitor_lp@yahoo.com>
2014-05-17 15:59:03 -0300

    Teste método q devolve ids de cada filtros filho

--- testes/FiltroTest.php
+++ testes/FiltroTest.php
@@ -16,4 +16,24 @@ class FiltroTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(0, $filtro->getId());
     }
 
+    public function testGetListaIdsFilhos()
+    {
+        $filtro = new Filtro(4, 'Matemática');
+        $this->assertEquals(array(), $filtro->getListaIdsFilhos());
+        
+        $f1 = new Filtro(3, 'Estatística');
+        $filtro->adicionaFilho($f1);
+        $e = array(3);
+        $this->assertEquals($e, $filtro->getListaIdsFilhos());
+        $f2 = new Filtro(5, 'Probabilidade');
+        $f2->adicionaFilho(new Filtro(6, 'Esperança'));
+        $f3 = new Filtro(16, 'Baysiano');
+        $f3->adicionaFilho(new Filtro(116, 'sub-baysiano'));
+        $f1->adicionaFilho(new Filtro(8, 'Mediana'));
+        $f1->adicionaFilho(new Filtro(18,'Amostragem'));
+        $f1->adicionaFilho($f3);
+        $filtro->adicionaFilho($f2);
+        $esperado = array(4,3,5,6,16,116,8,18);
+        $this->assertEquals(sort($esperado), sort($filtro->getListaIdsFilhos()));
+    }
 }
