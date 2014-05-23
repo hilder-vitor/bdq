b49bea3f561627ea01d80cc0a36571522a959ec2
vitorlima <vitor_lp@yahoo.com>
2014-05-23 10:58:13 -0300

    Agora é possível buscar os filhos de um filtro
    Por exemplo, se o cliente quiser os filtros filhos do 'Vestibular', basta acessar a URL
    
    <nome_base>/bdq/fonte/api/getFiltro.php?query=filhos&idFiltroPai=1261
    
    (#1)

--- fonte/api/getFiltro.php
+++ fonte/api/getFiltro.php
@@ -9,19 +9,49 @@ use classes_\BDManager;
  * ----------------------------------------------------------  */
 
 
+// ----------------- DEFININDO FUNÇÕES USADAS NESTA PÁGINA ----------------
+function devolveJsonFiltrosFilhos($idFiltroPai){
+    $bd = new BDManager();
+    $filtros = $bd->selecionaFiltro(null, null, $idFiltroPai,false);
+    $jsonFiltros = array();
+    foreach($filtros as $filtro){
+        $jsonFiltros[] = transformaFiltroEmArray($filtro);
+    }
+    exit(json_encode(array('status' => 0,
+		      'msg' => 'OK',
+		      'filtros' => $jsonFiltros)));
+}
+
+function transformaFiltroEmArray(Filtro $filtro){
+    return array('idFiltro' => $filtro->getId(),
+	       'nome' => $filtro->getNome(),
+	       'idPai' => $filtro->getIdPai());
+}
+
+// --------------  FIM DA DEFINIÇÃO DE FUNÇÕES --------------------
+
 if (!isset($_GET['query'])){
     exit(json_encode(array('status' => 1, 'msg' => 'Query não foi passada.')));
 }
+
+// extrai as variáveis passadas por GET
 extract($_GET);
 
-if ($query == 'primeiroNivel'){
-    $bd = new BDManager();
-    $filtros = $bd->selecionaFiltro(null, null, 0,false);
-    $jsonFiltros = array();
-    foreach($filtros as $filtro){
-        $jsonFiltros[] = array('idFiltro' => $filtro->getId(),
-			     'nome' => $filtro->getNome(),
-			     'idPai' => 0);
-    }
-    exit(json_encode($jsonFiltros));
+// se está pedindo os filtros do primeiro nível
+if (preg_match('/^primeiro_?nivel$/i', $query)){
+    devolveJsonFiltrosFilhos(0);
 }
+
+
+if (preg_match('/^filhos$/i', $query)){
+    if (!isset($idFiltroPai)){
+        exit(json_encode(array('status' => 3,
+		      'msg' => 'O id do filtro pai não foi passado.')));
+    }
+    devolveJsonFiltrosFilhos($idFiltroPai);
+}
+
+exit(json_encode(array('status' => 2,
+		      'msg' => 'A query deve ser uma das seguintes:'
+			     . ' "primeiroNivel",'
+			     . ' "filhos"')));
