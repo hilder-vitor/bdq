1341d5ef4a0c0aa8b2c22c6f9caa2af9d1ee0ecd
vitorlima <vitor_lp@yahoo.com>
2014-05-24 20:12:20 -0300

    Agora pode-se escolher o tipo de questão na busca
    A paǵina getQuestão agora aceita um parâmetro tipo para dizer se queremos questões
    dissertativas ou alternativas.  (#3)

--- fonte/api/getQuestao.php
+++ fonte/api/getQuestao.php
@@ -1,7 +1,8 @@
 <?php  require_once '../../autoloader.php';
 
-use \classes_\BDManager as BDManager;
+use classes_\BDManager as BDManager;
 use classes_\Json as Json;
+use classes_\Questao;
 
 /* ----------------------------------------------------------
  *      Página que recebe requisições dos clientes e busca
@@ -23,6 +24,18 @@ if (isset($idsFiltros)){
 	   $qntQuestoes = 100;
         }
     }
+    if (isset($tipo)){
+        if (preg_match('/^dissertativa$/i', $tipo)){
+	   $tipo = Questao::QUESTAO_DISSERTATIVA;
+        }else if (preg_match('/^teste$/i', $tipo)
+		|| preg_match('/^alternativa$/i', $tipo)){
+	   $tipo = Questao::QUESTAO_ALTERNATIVA;
+        }else{
+	   $tipo = null;
+        }
+    }else{
+        $tipo = null;
+    }
     
     $bd = new BDManager();
     $vetQuestoes = array();
@@ -35,6 +48,8 @@ if (isset($idsFiltros)){
 	   $filtros[] = $filtro;
         }
     }
-    $questoes = $bd->selecionaQuestao($idInicial, $qntQuestoes, $filtros);
+    echo "selecionaQuestao($idInicial, $qntQuestoes, $filtros,$tipo);<br>";
+    $questoes = $bd->selecionaQuestao($idInicial, $qntQuestoes, $filtros,$tipo);
+    
     exit(Json::transformaVetorDeQuestoesEmJson($questoes));    
 }
